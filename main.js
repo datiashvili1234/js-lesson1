7cba25651d38d90721270fafc9a5c92ae8684252
datiashvili daviti <davitdatiashvili3@gmail.com>
2025-02-23 15:07:36 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const name ="giorgi"
+const age1 = 21
+const num1 = 29
+const num2 = 19
+const num3 = 29 + 19
+const num4 = 55 - 50
+const num5 = 55 * 40
+const num6 = 55 / 50
+console.log( num5)
+console.log(num6)
+const num7 = num1 + num2
+const num8 = num4 + 
+console.log (num7)
+const h1 = document.querySelector(".h1")
+console.log(h1)
+
+const p1 =  document.querySelector(".p1")
+console.log(p1)
+h1.style.color = "blue"
+h1.style.border = "2px dashed green"
+h1.style.backgroundColor = "black"
+h1.style.fontSize = "100px"
+h1.remove()
